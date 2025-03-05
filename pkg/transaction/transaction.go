5b89519386162edd5491400086cb6343e0ce4113
Matthew-Mak <praymome46809@gmail.com>
2025-03-05 17:07:31 +0500

    some commit

---
+++ pkg/transaction/transaction.go
@@ -0,0 +1,6 @@
+package transaction
+
+import transactions "github.com/Matthew-Mak/card-Matthew-Mak/v2/pkg/types/transaction"
+
+func SaveTransaction(CardId int, transaction transactions.Transaction, transactionsMap map[int][]transactions.Transaction) map[int][]transactions.Transaction {
+}
